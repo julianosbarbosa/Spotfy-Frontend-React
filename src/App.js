e1c7c4cf9235fc7443643054b7604dd2bf559dab
Juliano <julianotrabalho2016@gmail.com>
2019-11-05 13:09:55 -0300

    UPDATE - browse layout component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/Global";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import { Wrapper, Container, Content } from "./styles/Components";
 import Header from "./components/Header";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <Wrapper>
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-        </Content>
-      </Container>
-
-      <Player />
-      <GlobalStyle />
-    </Wrapper>
+    <BrowserRouter>
+      <Wrapper>
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+        <GlobalStyle />
+      </Wrapper>
+    </BrowserRouter>
   );
 }
 
