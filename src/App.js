8b02e53f17c9b23c8484b541123f16fa13b382a4
Juliano <julianotrabalho2016@gmail.com>
2019-11-06 19:22:26 -0300

    UPDATE - configuration redux and saga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./config/reactotron";
 import GlobalStyle from "./styles/Global";
@@ -7,22 +8,25 @@ import Player from "./components/Player";
 import { Wrapper, Container, Content } from "./styles/Components";
 import Header from "./components/Header";
 import Routes from "./Routes";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-        <GlobalStyle />
-      </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+          <GlobalStyle />
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
